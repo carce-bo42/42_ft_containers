a1124b3fe187b67f4e353e579bfc6854dd33f3fa
Christian Arce! <yerildtt@gmail.com>
2022-10-02 16:51:59 +0200

    Simplified tests with better iterator syntax

--- test/vector/insert.cpp
+++ test/vector/insert.cpp
@@ -27,18 +27,14 @@ int insert_single_test() {
 {
   ft::vector<int> ft_vec(3, 4); // 4 4 4 
   ft_vec.push_back(3);
-  ft::vector<int>::iterator it = ft_vec.begin(); // ft_vec = 4 4 4 3
-  ++it;
-  ft_vec.insert(it, 12);
+  ft_vec.insert(ft_vec.begin() + 1, 12);
   // shoudl be now 4 12 4 4 3
   // iterator invalidated !.
   ft_vec.insert(ft_vec.begin(), 21);
   //print_vector<int>(vec_1);
   std::vector<int> std_vec(3, 4); // 4 4 4 
   std_vec.push_back(3);
-  std::vector<int>::iterator _it = std_vec.begin(); // std_vec = 4 4 4 3
-  ++_it;
-  std_vec.insert(_it, 12);
+  std_vec.insert(std_vec.begin() + 1, 12);
   // shoudl be now 4 12 4 4 3
   // iterator invalidated !.
   std_vec.insert(std_vec.begin(), 21);
@@ -50,14 +46,12 @@ int insert_single_test() {
   ft::vector<int> ft_vec(4, 5); // 5 5 5 5 
   for (int i = 0; i < 10; i++) {
     ft::vector<int>::iterator it = ft_vec.begin();
-    for(; it != ft_vec.end(); it++) {}
-    ft_vec.insert(it, 21);
+    ft_vec.insert(it + ft_vec.size(), 21);
   }
   std::vector<int> std_vec(4, 5); // 5 5 5 5 
   for (int i = 0; i < 10; i++) {
     std::vector<int>::iterator it = std_vec.begin();
-    for(; it != std_vec.end(); it++) {}
-    std_vec.insert(it, 21);
+    std_vec.insert(it + std_vec.size(), 21);
   }
   if (!After_Insert_Check(std_vec, ft_vec)) {
     return TEST_ERROR(KO_INSERT);
@@ -65,14 +59,10 @@ int insert_single_test() {
 }
 { // insert several at last position at once
   ft::vector<int> ft_vec(4, 5); // 5 5 5 5 
-  ft::vector<int>::iterator it = ft_vec.begin();
-  for(; it != ft_vec.end(); it++) {}
-  ft_vec.insert(it, 10, 21);
+  ft_vec.insert(ft_vec.begin() + ft_vec.size(), 10, 21);
   //print_vector(ft_vec);  
   std::vector<int> std_vec(4, 5); // 5 5 5 5 
-  std::vector<int>::iterator it_ = std_vec.begin();
-  for(; it_ != std_vec.end(); it_++) {}
-  std_vec.insert(it_, 10, 21);
+  std_vec.insert(std_vec.begin() + std_vec.size(), 10, 21);
   if (!After_Insert_Check(std_vec, ft_vec)) {
     return TEST_ERROR(KO_INSERT);
   }
@@ -80,18 +70,12 @@ int insert_single_test() {
 { // insert several before last position by inserting 1 each time
   ft::vector<int> ft_vec(4, 5); // 5 5 5 5 
   for (int i = 0; i < 10; i++) {
-    ft::vector<int>::iterator it = ft_vec.begin();
-    for(; it != ft_vec.end(); it++) {}
-    it--;
-    ft_vec.insert(it, 21);
+    ft_vec.insert(ft_vec.begin() + ft_vec.size() - 1, 21);
   }
   //print_vector(ft_vec);
   std::vector<int> std_vec(4, 5); // 5 5 5 5 
   for (int i = 0; i < 10; i++) {
-    std::vector<int>::iterator it = std_vec.begin();
-    for(; it != std_vec.end(); it++) {}
-    it--;
-    std_vec.insert(it, 21);
+    std_vec.insert(std_vec.begin() + std_vec.size() - 1, 21);
   }
   if (!After_Insert_Check(std_vec, ft_vec)) {
     return TEST_ERROR(KO_INSERT);
@@ -102,18 +86,12 @@ int insert_single_test() {
   ft::vector<int> ft_vec(4, 5); // 5 5 5 5 
   ft_vec.reserve(200);
   for (int i = 0; i < 194; i++) {
-    ft::vector<int>::iterator it = ft_vec.begin();
-    for(; it != ft_vec.end(); it++) {}
-    it--;
-    ft_vec.insert(it, 21);
+    ft_vec.insert(ft_vec.begin() + ft_vec.size() - 1, 21);
   }
   std::vector<int> std_vec(4,5);
   std_vec.reserve(200);
   for (int i = 0; i < 194; i++) {
-    std::vector<int>::iterator it = std_vec.begin();
-    for(; it != std_vec.end(); it++) {}
-    it--;
-    std_vec.insert(it, 21);
+    std_vec.insert(std_vec.begin() + std_vec.size() - 1, 21);
   }
   if (!After_Insert_Check(std_vec, ft_vec)) {
     return TEST_ERROR(KO_INSERT);
@@ -125,19 +103,13 @@ int insert_single_test() {
   ft::vector<int> ft_vec(4, 5); // 5 5 5 5 
   ft_vec.reserve(200);
   for (int i = 0; i < 194; i++) {
-    ft::vector<int>::iterator it = ft_vec.begin();
-    for(; it != ft_vec.end(); it++) {}
-    it--;
-    ft_vec.insert(it, 21);
+    ft_vec.insert(ft_vec.begin() + ft_vec.size() - 1, 21);
   }
   //print_vector(vec);
   std::vector<int> std_vec(4, 5); // 5 5 5 5 
   std_vec.reserve(200);
   for (int i = 0; i < 194; i++) {
-    std::vector<int>::iterator it = std_vec.begin();
-    for(; it != std_vec.end(); it++) {}
-    it--;
-    std_vec.insert(it, 21);
+    std_vec.insert(std_vec.begin() + std_vec.size() - 1, 21);
   }
   if (!After_Insert_Check(std_vec, ft_vec)) {
     return TEST_ERROR(KO_INSERT);
@@ -151,9 +123,7 @@ int insert_single_test() {
   ft_vec_input.push_back(8);
   ft_vec_input.push_back(7);
 
-  ft::vector<int>::iterator it = ft_vec.end();
-  --it;
-  ft_vec.insert(it, ft_vec_input.begin(), ft_vec_input.end());
+  ft_vec.insert(ft_vec.end() - 1, ft_vec_input.begin(), ft_vec_input.end());
 
   std::vector<int> std_vec(10, 3); // 3 3 3 3 3 3 3 3 3 3
   std::vector<int> std_vec_input;
@@ -162,9 +132,7 @@ int insert_single_test() {
   std_vec_input.push_back(8);
   std_vec_input.push_back(7);
 
-  std::vector<int>::iterator it_ = std_vec.end();
-  --it_;
-  std_vec.insert(it_, std_vec_input.begin(), std_vec_input.end());
+  std_vec.insert(std_vec.end() - 1, std_vec_input.begin(), std_vec_input.end());
 
   //print_vector(ft_vec);
   if (!After_Insert_Check(std_vec, ft_vec)) {
