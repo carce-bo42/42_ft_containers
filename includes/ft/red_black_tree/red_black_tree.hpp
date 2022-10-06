f4b647c8710fd7c49ed58daee92d19659a6fe688
Christian Arce! <yerildtt@gmail.com>
2022-10-06 21:06:29 +0200

    Rb tree interface thought

--- includes/ft/red_black_tree/red_black_tree.hpp
+++ includes/ft/red_black_tree/red_black_tree.hpp
@@ -2,16 +2,71 @@
 # define CONTAINERS_RED_BLACK_TREE_HPP
 
 #include <memory>
+#include <functional>
 #include "ft/red_black_tree/node.hpp"
-
 
 namespace ft {
 
 template < typename Key,
-           typename Val,
-           typename Compare, // functor
-           typename Alloc = std::allocator<Val> >
+           typename Val, // Val = Pair<Key, T> T typename
+           typename KeyOfVal, // In stl, this is used presumably to generalize
+                              // Val to any structure containing Key. With this
+                              // functor you are supposed to operate on Val
+                              // types to extract Keys.
+           typename Compare = std::less<Key>, // functor
+           typename Alloc = std::allocator<Val> > 
 class rb_tree {
+
+  protected:
+  
+  typedef typename rb_tree_node<T>::node_ptr       node_ptr;
+  typedef typename rb_tree_node<T>::const_node_ptr const_node_ptr;
+  typedef typename rb_tree_node<T>::node_color     node_color;
+
+  private:
+
+  node_ptr root;
+  size_t   node_count;
+  // possibly missing some variables for now.
+  // balancing will be the last thing to do.
+
+  public:
+
+  typedef Key 				      key_type;
+  typedef Val 				      value_type;
+  typedef value_type*       pointer;
+  typedef const value_type* const_pointer;
+  typedef value_type& 			reference;
+  typedef const value_type& const_reference;
+  typedef size_t 				    size_type;
+  typedef ptrdiff_t 			  difference_type;
+  typedef Alloc 				    allocator_type;
+
+/*
+ * red black tree interface  
+ * 
+ * get_node_from_key()
+ * get_node_key()
+ * get_node_data()
+ * construct_node()
+ * destroy_node()
+ * search_node() given a key
+ * get_minimum()
+ * get_maximum()
+ * /////// iterators go somewhere else
+ * pure_insert() - insert a node on the tree 
+ * pure_erase() - erase a node from the tree
+ * after_insert_check() : will determine if a newly inserted
+ *                        node is a problem and which problem
+ *                        is it.
+ * after_erase_check() : same but with deletion, in case theres
+ *                       diferent conditions met. But will somehow
+ *                       redirect it to rebalance in a way rebalance
+ *                       is agnostic to wether an insertion or 
+ *                       deletion happened.
+ * rebalance() : will recieve a node and a problem to solve.
+ *
+ */
 
 
 
