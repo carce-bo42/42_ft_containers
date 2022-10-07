0564b209f78c1e79a7156e3c43db25ce751a7ee0
Christian Arce! <yerildtt@gmail.com>
2022-10-07 16:16:28 +0200

    added more to the node base interface

--- includes/ft/RBT/red_black_tree.hpp
+++ includes/ft/RBT/red_black_tree.hpp
@@ -33,15 +33,15 @@ class rb_tree {
 
   public:
 
-  typedef Key 				      key_type;
-  typedef Val 				      value_type;
+  typedef Key               key_type;
+  typedef Val               value_type;
   typedef value_type*       pointer;
   typedef const value_type* const_pointer;
-  typedef value_type& 			reference;
+  typedef value_type&       reference;
   typedef const value_type& const_reference;
-  typedef size_t 				    size_type;
-  typedef ptrdiff_t 			  difference_type;
-  typedef Alloc 				    allocator_type;
+  typedef size_t            size_type;
+  typedef ptrdiff_t         difference_type;
+  typedef Alloc             allocator_type;
 
 /*
  * red black tree interface  
--- includes/ft/RBT/red_black_tree_node.hpp
+++ includes/ft/RBT/red_black_tree_node.hpp
@@ -7,6 +7,36 @@ namespace ft {
  */
 enum rb_tree_node_color { red = false, black = true };
 
+enum rb_tree_node_orientation { right_child = false, left_child = true };
+
+enum rb_tree_node_nillness { nil = true, not_nil = false };
+
+// This will allow to separate what is importnant for 
+// balancing from the rest of the node information.
+// allowing declarations of the type
+// node( ..., red, right_child, nil).
+struct rb_tree_node_properties {
+
+  typedef rb_tree_node_properties  self;
+  typedef rb_tree_node_color       n_color;
+  typedef rb_tree_node_orientation n_orientation;
+  typedef rb_tree_node_nillness    nilness;
+
+  n_color       color;
+  n_orientation orientation;
+  nilness      nil;
+
+  rb_tree_node_properties(n_color color,
+                          n_orientation orientation,
+                          nilness nil)
+  :
+    color(color),
+    orientation(orientation),
+    nil(nil)
+  {}
+};
+
+
 /*
  * A node template from a red-black tree. T will
  * always be pairs in case of the map. 
@@ -14,16 +44,17 @@ enum rb_tree_node_color { red = false, black = true };
 template <typename T>
 struct rb_tree_node {
 
-  typedef T                      value_type;
-  typedef rb_tree_node*          node_ptr;
-  typedef const rb_tree_node*    const_node_ptr;
-  typedef rb_tree_node_color     node_color;
+  typedef T                       value_type;
+  typedef rb_tree_node*           node_ptr;
+  typedef const rb_tree_node*     const_node_ptr;
+  typedef rb_tree_node_color      node_color;
+  typedef rb_tree_node_properties node_properties;
   
-  node_ptr   parent; // needed for balancing
-  node_ptr   left;
-  node_ptr   right;
-  node_color color;
-  value_type data;
+  node_ptr         parent; 
+  node_ptr         left;
+  node_ptr         right;
+  value_type       data;
+  node_properties  properties;
 
   // empty constructor
   rb_tree_node(const T& value = T())
@@ -31,8 +62,8 @@ struct rb_tree_node {
     parent(),
     left(),
     right(),
-    color(black),
-    data(value)
+    data(value),
+    properties()
   {}
 
   rb_tree_node(const T& value, const node_ptr parent)
@@ -40,6 +71,21 @@ struct rb_tree_node {
     parent(parent),
     data(value)
   {}
+
+  /*
+   * Rotations depend on wether a child is at the right
+   * or at the left of the parent, so these methods are
+   * necessary.
+   */
+  /*
+  bool is_left_child() {
+
+
+  }
+
+  bool is_right_child() {
+
+  }*/
 
 }; // struct rb_tree_node
 
